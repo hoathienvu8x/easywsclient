c88193dd0a5031a10255f1b2992122101af53095
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2024-05-15 13:21:48 +0700

    Add random key handshake

--- easywsclient.cpp
+++ easywsclient.cpp
@@ -484,6 +484,48 @@ namespace { // private module-only namespace
       _RealWebSocket& operator=(const _RealWebSocket&);
   };
 
+  static const std::string base64_chars =
+             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+             "abcdefghijklmnopqrstuvwxyz"
+             "0123456789+/";
+
+  std::string base64_encode(
+    unsigned char const* bytes_to_encode, unsigned int in_len
+  ) {
+    std::string ret;
+    int i = 0;
+    int j = 0;
+    unsigned char char_array_3[3];
+    unsigned char char_array_4[4];
+
+    while (in_len--) {
+      char_array_3[i++] = *(bytes_to_encode++);
+      if (i == 3) {
+        char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+        char_array_4[3] = char_array_3[2] & 0x3f;
+
+        for(i = 0; (i <4) ; i++) ret += base64_chars[char_array_4[i]];
+        i = 0;
+      }
+    }
+
+    if (i) {
+      for(j = i; j < 3; j++) char_array_3[j] = '\0';
+
+      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+      char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+      char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+      char_array_4[3] = char_array_3[2] & 0x3f;
+
+      for (j = 0; (j < i + 1); j++) ret += base64_chars[char_array_4[j]];
+
+      while((i++ < 3)) ret += '=';
+    }
+
+    return ret;
+  }
 
   easywsclient::WebSocket::pointer from_url(
     const std::string& url, bool useMask, const std::string& origin
@@ -549,7 +591,13 @@ namespace { // private module-only namespace
         snprintf(line, 1024, "Origin: %s\r\n", origin.c_str());
         ::send(sockfd, line, strlen(line), 0);
       }
-      snprintf(line, 1024, "Sec-WebSocket-Key: x3JJHMbDL1EzLkh9GBhXDw==\r\n");
+      unsigned char key_nonce[16];
+      srand(time(NULL));
+      for(int z = 0; z < 16; z++) {
+          key_nonce[z] = rand() & 0xff;
+      }
+      std::string websocket_key = base64_encode(key_nonce, 16);
+      snprintf(line, 1024, "Sec-WebSocket-Key: %s\r\n", websocket_key.c_str());
       ::send(sockfd, line, strlen(line), 0);
       snprintf(line, 1024, "Sec-WebSocket-Version: 13\r\n");
       ::send(sockfd, line, strlen(line), 0);
